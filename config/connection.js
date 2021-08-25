82afa5ceb7c56f2c6d83e2cc7f184f0ce332cd8e
Vashti <patrena@sbcglobal.net>
2021-08-25 14:27:01 -0700

    updated structure

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
+// import the Sequelize constructor from the library
+const { DatabaseError } = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
- 
-const sequelize = process.env.JAWSDB_URL 
-? new Sequelize(process.env.JAWSDB_URL) 
-: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+// create connection to our DatabaseError, pas in your MYSQL information for username and password
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
     });
-
-module.exports = sequelize;
+  }
+  
+  module.exports = sequelize;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { findAll } = require('../../models/User');
+const { User, Post, Comment, Vote } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
