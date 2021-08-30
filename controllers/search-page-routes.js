84b5a0bf0aaeb8c783c22997feb8ebc35bebfba0
Akeem C <javantieclarke@gmail.com>
2021-08-30 19:16:24 -0400

    updated login page

--- controllers/search-page-routes.js
+++ controllers/search-page-routes.js
@@ -1,69 +1,87 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Vote } = require("../models");
 
-
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
-
-
-router.get('/', (req,res)=>{
-    console.log('======================');
-    res.render('search')
-    
-    
-})
+router.get("/", (req, res) => {
+  console.log("======================");
+  res.render("search");
+});
 
 // get all users
-router.get('/topCommenters', (req, res) => {
-    console.log('======================');
-    console.log('Search Top Commenters');
-    User.findAll({
-      attributes: [
-          'id', 
-          'username', 
-          [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE comment.user_id = user.id)'), 'num_comments']
-        ],
-      
-      include:[{
-          model: Comment,
-          attributes:[],
-          include: []
-      }],
-       order: [[sequelize.literal('num_comments'), 'DESC'], [sequelize.literal('User.id'), 'ASC']],
-    }
-    )
-      .then(dbCommentData => {
-        const topCommenters = dbCommentData.map(comment => comment.get({ plain: true }))
-        console.log(topCommenters)
-        res.render('search', topCommenters)})
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.get("/topCommenters", (req, res) => {
+  console.log("======================");
+  console.log("Search Top Commenters");
+  User.findAll({
+    attributes: [
+      "id",
+      "username",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM comment WHERE comment.user_id = user.id)"
+        ),
+        "num_comments",
+      ],
+    ],
 
-  router.get('/topCreators', (req, res) => {
-    console.log('======================');
-    console.log('Search Top Creators');
-    User.findAll({
-      attributes: [
-          'id', 
-          'username', 
-          [sequelize.literal('(SELECT COUNT(*) FROM post WHERE post.user_id = user.id)'), 'num_posts']
-        ],
-      
-      include:[{
-          model: Post,
-          attributes:[],
-          include: []
-      }],
-       order: [[sequelize.literal('num_posts','User.id'), 'DESC'], [sequelize.literal('User.id'), 'ASC']],
-    }
-    )
-      .then(dbPostData => res.render('search'))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    include: [
+      {
+        model: Comment,
+        attributes: [],
+        include: [],
+      },
+    ],
+    order: [
+      [sequelize.literal("num_comments"), "DESC"],
+      [sequelize.literal("User.id"), "ASC"],
+    ],
+  })
+    .then((dbCommentData) => {
+      const topCommenters = dbCommentData.map((comment) =>
+        comment.get({ plain: true })
+      );
+      console.log(topCommenters);
+      res.render("search", {
+       topCommenters,
       });
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/topCreators", (req, res) => {
+  console.log("======================");
+  console.log("Search Top Creators");
+  User.findAll({
+    attributes: [
+      "id",
+      "username",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM post WHERE post.user_id = user.id)"
+        ),
+        "num_posts",
+      ],
+    ],
+
+    include: [
+      {
+        model: Post,
+        attributes: [],
+        include: [],
+      },
+    ],
+    order: [
+      [sequelize.literal("num_posts", "User.id"), "DESC"],
+      [sequelize.literal("User.id"), "ASC"],
+    ],
+  })
+    .then((dbPostData) => res.render("search"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
